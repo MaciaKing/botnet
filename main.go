5c9186ad5776e50fa89bdbf2688643bdac082627
MaciaKing <maciasalvasalva@gmail.com>
2024-08-10 19:16:09 +0200

    [feat]: template implemented The template implemented is https://themewagon.github.io/kaiadmin-lite/

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"botnet/database"
 	"botnet/handler"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,5 +20,18 @@ func main() {
 	})
 
 	r.GET("/bots", handler.GetAllBots)
+
+	// r.LoadHTMLGlob("./templates/*")
+	r.LoadHTMLGlob("./templates/*.html")
+
+	// Servir archivos estáticos desde `templates/assets`
+	r.Static("/assets", "./templates/assets")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Main website",
+		})
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
